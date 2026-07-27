ff2b3a9a44f4952bdad28bf91717450ca5f84f9b
Murilo <MuriloT_Costa@hotmail.com>
2026-07-26 22:42:55 -0300

    Configurando o filtro JSON de laudos

--- app/Http/Resources/LaudoResources.php
+++ app/Http/Resources/LaudoResources.php
@@ -5,15 +5,18 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+//Classe que é respnsavel por criar a formatação padão para os elementos JSON
+//Essa classe, permite tratar e filtrar os campos que irão aparecer no JSON
 class LaudoResources extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+    //Função responsavel por fazer a formatação
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        //Esse função retorna um array associativo com os campos filtrados
+        return [
+            'id' => $this->pk_id_laudos, //Envia o id do laudo
+            'laudo' => $this->nome_laudo, //Envia o nome do laudo
+            'caminho' => $this->caminho_laudo, //Envia o caminho do laudo
+        ];
     }
 }
